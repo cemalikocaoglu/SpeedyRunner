1987e7140e28fe26a40570d7f032002197e9bbe1
cemalikocaoglu <83187104+cemalikocaoglu@users.noreply.github.com>
2023-07-15 20:50:08 +0300

    Horizontal moving Input System settings

--- Assets/Scripts/Concrates/Controllers/PlayerControllers.cs
+++ Assets/Scripts/Concrates/Controllers/PlayerControllers.cs
@@ -1,8 +1,10 @@
+using SpeedyRunner.Abstract.Inputs;
+using SpeedyRunner.Inputs;
 using SpeedyRunner.Movments;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 namespace SpeedyRunner.Controllers
 {
@@ -23,18 +25,34 @@ namespace SpeedyRunner.Controllers
 
 
         public bool _jump;
+        float _horizontal;
+
+
+
+        IInputReader _input;
 
         private void Awake()
         {
             _horizontalMovers = new HorizontalMovers(this);
 
             _jumpRigidbody = new JumpRigidbody(this);
+
+            _input = new InputReader(GetComponent<PlayerInput>());
         }
+
+
+        private void Update()
+        {
+            _horizontal = _input.Horizontal;
+
+
+        }
+
 
 
         private void FixedUpdate()
         {
-            _horizontalMovers.tickFixed(horizontal , speed);
+            _horizontalMovers.tickFixed(_horizontal, speed);
 
             if( _jump )
             {
--- Assets/Scripts/Concrates/Input/InputReader.cs
+++ Assets/Scripts/Concrates/Input/InputReader.cs
@@ -1,18 +1,33 @@
+using SpeedyRunner.Abstract.Inputs;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputReader : MonoBehaviour
+namespace SpeedyRunner.Inputs
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InputReader : IInputReader
     {
-        
+        PlayerInput _playerInput;
+
+
+        public float Horizontal { get; private set; }
+
+
+        public InputReader(PlayerInput playerInput)
+        {
+
+            _playerInput = playerInput;
+
+            _playerInput.currentActionMap.actions[0].performed += OnHorizontalMove;
+                    
+        }
+
+        private void OnHorizontalMove(InputAction.CallbackContext context)
+        {
+            Horizontal = context.ReadValue<float>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
